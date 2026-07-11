0bf143d35e488bb0e6706e6dd02577a09e0ebcc0
pratik-khairnar <pratik.251105@gmail.com>
2026-07-11 17:55:23 +0530

    Added CORS middleware in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,11 +6,25 @@ from app.api.v1.users import router as user_router
 from app.api.v1.images import router as image_router
 from app.api.v1.workspaces import router as workspace_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Virtual Workbench Backend",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
